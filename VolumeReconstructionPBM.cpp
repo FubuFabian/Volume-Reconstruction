87610ac23bc05fcafcb6aeaa98d6939f3a34cdf9
Fabian Torres <dae.wong@gmail.com>
2016-10-26 15:30:19 -0500

    PBM grow

--- VolumeReconstructionPBM.cpp
+++ VolumeReconstructionPBM.cpp
@@ -1,6 +1,7 @@
 #include "VolumeReconstructionPBM.h"
 
-#include "time.h"
+#include <time.h>
+#include <math.h>
 #include <vtkMath.h>
 
 vtkSmartPointer<vtkImageData> VolumeReconstructionPBM::generateVolume()
@@ -133,7 +134,7 @@ void VolumeReconstructionPBM::binFilling()
     clock_t end = clock();
     double diffticks = end - begin;
     double diffms = (diffticks * 10) / CLOCKS_PER_SEC;
-    std::cout<<std::endl<<"Time elapsed reconstructing volume: "<< double(diffms)<<" ms" <<std::endl;
+    std::cout<<std::endl<<"Time elapsed reconstructing volume: "<< double(diffms)<<" ms" <<std::end;
 
 }
 
@@ -144,118 +145,78 @@ void VolumeReconstructionPBM::holeFillingFixedRegion()
 	int windowStep = vtkMath::Floor(kernelSize/2);
     
     double maxDistance = calcMaxDistance();
-    std::cout<<"Filling Empty Voxels with a Kernel of "<<kernelSize<<"x"<<kernelSize<<std::endl;
+    std::cout<<"Filling Empty Voxels with a max Kernel of "<<kernelSize<<"x"<<kernelSize<<std::endl;
     
     clock_t begin = clock();
 
-    // create an interpolation buffer
-    vtkSmartPointer<vtkImageData> InterpolationVolume = vtkSmartPointer<vtkImageData>::New();
-    InterpolationVolume->SetScalarTypeToUnsignedChar();
-    InterpolationVolume->SetNumberOfScalarComponents(1);
-    InterpolationVolume->SetOrigin(volumeOrigin[0],volumeOrigin[1],volumeOrigin[2]);
-    InterpolationVolume->SetDimensions(volumeSize[0],volumeSize[1],volumeSize[2]);
-    InterpolationVolume->SetSpacing(scale[0]*resolution,scale[1]*resolution,scale[1]*resolution);
-    InterpolationVolume->AllocateScalars();   
-    
-    unsigned char * interpolationVoxel;
-    
-    for(int i=0; i<volumeSize[0]; i++){
-	for(int j=0; j<volumeSize[1]; j++){
-		for(int k=0; k<volumeSize[2]; k++){
-                    
-                    // get pointer to the current volume voxel   
-                    interpolationVoxel = static_cast<unsigned char *> (
-                            InterpolationVolume->GetScalarPointer(i,j,k));                   
-                    
-                    interpolationVoxel[0] = 0;                                    
-                }
-        }
-    }
-
-
     // create volume pointers
-    unsigned char * voxel;
     unsigned char * filledVoxel;
     unsigned char * innerVoxel;
-    
-    vnl_vector<double> centralVoxel;
-    centralVoxel.set_size(3);
-    
-    vnl_vector<double> foundVoxel;
-    foundVoxel.set_size(3);
 
     for(int x=0; x<volumeSize[0]; x++){
         
 	std::cout<<"."<<std::flush;
 
-	for(int y=0; y<volumeSize[1]; y++){
-		for(int z=0; z<volumeSize[2]; z++){
+		for(int y=0; y<volumeSize[1]; y++){
+			for(int z=0; z<volumeSize[2]; z++){
 
-                filledVoxel = static_cast<unsigned char *>
-                        (filledVolume->GetScalarPointer(x, y, z));
+					filledVoxel = static_cast<unsigned char *>
+							(filledVolume->GetScalarPointer(x, y, z));
 
-                if (filledVoxel[0] == 0)
-                {
-                    // center window point
-                    centralVoxel[0] = x;
-                    centralVoxel[1] = y;
-                    centralVoxel[2] = z;
-                        
-                    // create subvolume extent
-                    int subX1 = ((x - windowStep) < 0) ? 0 : x - windowStep;
-                    int subX2 = ((x + windowStep) >= volumeSize[0]) ? volumeSize[0]-1 : x + windowStep;
-                    int subY1 = ((y - windowStep) < 0) ? 0 : y - windowStep;
-                    int subY2 = ((y + windowStep) >= volumeSize[1]) ? volumeSize[1]-1 : y + windowStep;
-                    int subZ1 = ((z - windowStep) < 0) ? 0 : z - windowStep;
-                    int subZ2 = ((z + windowStep) >= volumeSize[2]) ? volumeSize[2]-1 : z + windowStep;
+					if (filledVoxel[0] == 0)
+					{                 
+						// create subvolume extent
+						int subX1 = ((x - windowStep) < 0) ? 0 : x - windowStep;
+						int subX2 = ((x + windowStep) >= volumeSize[0]) ? volumeSize[0]-1 : x + windowStep;
+						int subY1 = ((y - windowStep) < 0) ? 0 : y - windowStep;
+						int subY2 = ((y + windowStep) >= volumeSize[1]) ? volumeSize[1]-1 : y + windowStep;
+						int subZ1 = ((z - windowStep) < 0) ? 0 : z - windowStep;
+						int subZ2 = ((z + windowStep) >= volumeSize[2]) ? volumeSize[2]-1 : z + windowStep;
 
-                    for (int i = subX1; i <= subX2; i++){
-                        for (int j = subY1; j <= subY2; j++){
-                            for (int k = subZ1; k < subZ2; k++){
-                                
-                                if (x == i && y == j && z == k)
-                                    continue;
+						for (int i = subX1; i <= subX2; i++){
+							for (int j = subY1; j <= subY2; j++){
+								for (int k = subZ1; k < subZ2; k++){
+	                                
+									if (x == i && y == j && z == k)
+										continue;
 
-                                innerVoxel = static_cast<unsigned char *> (
-                                        volumeData->GetScalarPointer(i, j, k));
+									innerVoxel = static_cast<unsigned char *> (
+											filledVolume->GetScalarPointer(i,j,k));
 
-                                if (innerVoxel[0] != 0)
-                                {
-                                    foundVoxel[0] = i;
-                                    foundVoxel[1] = j;
-                                    foundVoxel[2] = k;
-                                    
-                                    double distance = vnl_vector_ssd(centralVoxel,foundVoxel);
-                                    
-                                    //double w = exp(-0.5*pow(distance/maxDistance,2.0)); //Gaus
-                                    double w = 1 - distance/maxDistance; //Lineal
+									if (innerVoxel[0] != 0)
+									{
+	                                    
+										double distance = sqrt((x-i)^2+(y-j)^2+(z-k)^2);
+	                                    
+										//double w = exp(-0.5*pow(distance/maxDistance,2.0)); //Gaus
+										double w = 1 - distance/maxDistance; //Lineal
 
-									// get pointer to the current volume voxel 
-									unsigned char *imageVoxel = static_cast<unsigned char *> (
-											volumeData->GetScalarPointer(foundVoxel[0], foundVoxel[1], foundVoxel[2]));
+										// get pointer to the current volume voxel 
+										unsigned char *imageVoxel = static_cast<unsigned char *> (
+												volumeData->GetScalarPointer(i,j,k));
 
-									// get pointer to the current volume voxel 
-									unsigned char * volumeVoxel = static_cast<unsigned char *> (
-											volumeData->GetScalarPointer(voxel[0], voxel[1], voxel[2]));
+										// get pointer to the current volume voxel 
+										unsigned char * volumeVoxel = static_cast<unsigned char *> (
+												volumeData->GetScalarPointer(x,y,z));
 
-									// get pointer to the current accumator volume voxel 
-									unsigned char * accDataVoxel = static_cast<unsigned char *> (
-											AccDataVolume->GetScalarPointer(voxel[0], voxel[1], voxel[2]));
-														
-									accDataVoxel[0]++;
+										// get pointer to the current accumator volume voxel 
+										unsigned char * accDataVoxel = static_cast<unsigned char *> (
+												AccDataVolume->GetScalarPointer(x,y,z);
+															
+										accDataVoxel[0]++;
 
-									float temp = volumeVoxel[0] + (w*imageVoxel[0]-volumeVoxel[0])/accDataVoxel[0];
-									volumeVoxel[0] = (unsigned char) temp;
+										float temp = volumeVoxel[0] + (w*imageVoxel[0]-volumeVoxel[0])/accDataVoxel[0];
+										volumeVoxel[0] = (unsigned char) temp;
 
-                                }
-                            }
-                        }
-                    }
+									}
+								}
+							}
+						}
 
-  
-                }
-            }
-        }
+	  
+					}
+				}
+			}
     }
 
     
@@ -263,6 +224,9 @@ void VolumeReconstructionPBM::holeFillingFixedRegion()
     double diffticks = end - begin;
     double diffms = (diffticks * 10) / CLOCKS_PER_SEC;
     std::cout<<std::endl<<"Time elapsed filling volume: "<< double(diffms)<<" ms" <<std::endl;
+
+	filledVolume->Delete();
+	AccDataVolume->Delete();
 
 }
 
@@ -279,42 +243,9 @@ void VolumeReconstructionPBM::holeFillingGrowingRegion()
     
     clock_t begin = clock();
 
-    // create an interpolation buffer
-    vtkSmartPointer<vtkImageData> InterpolationVolume = vtkSmartPointer<vtkImageData>::New();
-    InterpolationVolume->SetScalarTypeToUnsignedChar();
-    InterpolationVolume->SetNumberOfScalarComponents(1);
-    InterpolationVolume->SetOrigin(volumeOrigin[0],volumeOrigin[1],volumeOrigin[2]);
-    InterpolationVolume->SetDimensions(volumeSize[0],volumeSize[1],volumeSize[2]);
-    InterpolationVolume->SetSpacing(scale[0]*resolution,scale[1]*resolution,scale[1]*resolution);
-    InterpolationVolume->AllocateScalars();   
-    
-    unsigned char * interpolationVoxel;
-    
-    for(int i=0; i<volumeSize[0]; i++){
-	for(int j=0; j<volumeSize[1]; j++){
-		for(int k=0; k<volumeSize[2]; k++){
-                    
-                    // get pointer to the current volume voxel   
-                    interpolationVoxel = static_cast<unsigned char *> (
-                            InterpolationVolume->GetScalarPointer(i,j,k));                   
-                    
-                    interpolationVoxel[0] = 0;                                    
-                }
-        }
-    }
-
-
     // create volume pointers
-    unsigned char * voxel;
     unsigned char * filledVoxel;
     unsigned char * innerVoxel;
-    unsigned char * interpVoxel;
-    
-    vnl_vector<double> centralVoxel;
-    centralVoxel.set_size(3);
-    
-    vnl_vector<double> foundVoxel;
-    foundVoxel.set_size(3);
 
     for(int x=0; x<volumeSize[0]; x++){
         
@@ -328,15 +259,9 @@ void VolumeReconstructionPBM::holeFillingGrowingRegion()
 
                 if (filledVoxel[0] == 0)
                 {
-                    // center window point
-                    centralVoxel[0] = x;
-                    centralVoxel[1] = y;
-                    centralVoxel[2] = z;
-
                     // initial window size
                     int kernelSize = 3;
                     int windowStep = 1;
-                    float acc = 0;
                     int idx = 0;
 
                     while ((idx == 0) && (kernelSize <= maxKernelSize))
@@ -358,20 +283,31 @@ void VolumeReconstructionPBM::holeFillingGrowingRegion()
                                         continue;
 
                                     innerVoxel = static_cast<unsigned char *> (
-                                            volumeData->GetScalarPointer(i, j, k));
+                                            filledVolume->GetScalarPointer(i,j,k));
 
                                     if (innerVoxel[0] != 0)
-                                    {
-                                        foundVoxel[0] = i;
-                                        foundVoxel[1] = j;
-                                        foundVoxel[2] = k;
-                                        
-                                        double distance = vnl_vector_ssd(centralVoxel,foundVoxel);
-                                        
-                                        //double w = exp(-0.5*pow(distance/maxDistance,2.0)); //Gaus
-                                        double w = 1 - distance/maxDistance; //Lineal
+                                    {                         
+                                        double distance = sqrt((x-i)^2+(y-j)^2+(z-k)^2);
+	                                    
+										//double w = exp(-0.5*pow(distance/maxDistance,2.0)); //Gaus
+										double w = 1 - distance/maxDistance; //Lineal
 
-                                        acc += w*innerVoxel[0];
+										// get pointer to the current volume voxel 
+										unsigned char *imageVoxel = static_cast<unsigned char *> (
+												volumeData->GetScalarPointer(i,j,k));
+
+										// get pointer to the current volume voxel 
+										unsigned char * volumeVoxel = static_cast<unsigned char *> (
+												volumeData->GetScalarPointer(x,y,z));
+
+										// get pointer to the current accumator volume voxel 
+										unsigned char * accDataVoxel = static_cast<unsigned char *> (
+												AccDataVolume->GetScalarPointer(x,y,z);
+															
+										accDataVoxel[0]++;
+
+										float temp = volumeVoxel[0] + (w*imageVoxel[0]-volumeVoxel[0])/accDataVoxel[0];
+										volumeVoxel[0] = (unsigned char) temp;
 
                                         idx++;
                                     }
@@ -379,42 +315,11 @@ void VolumeReconstructionPBM::holeFillingGrowingRegion()
                             }
                         }
 
-                        if (idx != 0)
-                        {
-                            acc /= idx;
-
-                            interpVoxel = static_cast<unsigned char*>
-                                    (InterpolationVolume->GetScalarPointer(x, y, z));
-                            interpVoxel[0] = acc;
-                        }
-                        else
-                        {
+                        if (idx = 0){
                             windowStep++;
                             kernelSize += 2;
                         }
                     }
-                }
-            }
-        }
-    }
-
-
-    for(int x=0; x<volumeSize[0]; x++){
-	for(int y=0; y<volumeSize[1]; y++){
-		for(int z=0; z<volumeSize[2]; z++){
-                    
-                    
-                interpVoxel = static_cast<unsigned char*>
-                        (InterpolationVolume->GetScalarPointer(x, y, z));
-                
-                if (interpVoxel[0] != 0)
-                {
-                    voxel = static_cast<unsigned char *>
-                            (volumeData->GetScalarPointer(x, y, z));
-
-
-
-                    voxel[0] = interpVoxel[0];
                 }
             }
         }
@@ -424,6 +329,9 @@ void VolumeReconstructionPBM::holeFillingGrowingRegion()
     double diffticks = end - begin;
     double diffms = (diffticks * 10) / CLOCKS_PER_SEC;
     std::cout<<std::endl<<"Time elapsed filling volume: "<< double(diffms)<<" ms" <<std::endl;
+
+	filledVolume->Delete();
+	AccDataVolume->Delete();
 }
 
 void VolumeReconstructionPBM::setScale(vnl_vector<double> scale)
